8734b4fe70e65122b2901c50bbdce35bef5adc04
Dalibor Dragojevic <daco.dragojevic@gmail.com>
2017-04-10 18:28:56 +0200

    Dizajniranje izgleda aplikacije zapoceto.

--- src/app/kontrolna-tabla/meni-tabla/meni-tabla.component.ts
+++ src/app/kontrolna-tabla/meni-tabla/meni-tabla.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd }   from '@angular/router';
 
 /**
  * Prikazuje meni aplikacije
@@ -10,11 +11,40 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './meni-tabla.component.html',
   styleUrls: ['./meni-tabla.component.css']
 })
-export class MeniTablaComponent implements OnInit {
+export class MeniTablaComponent implements OnInit, OnDestroy {
+	izabranaOpcija: number;
+	unsubscribeRouterEvents;
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+  	this.unsubscribeRouterEvents = this.router.events.subscribe(val => {
+      if (val instanceof NavigationEnd) { // kraj navigacije
+        if (this.route.children[0].snapshot.url[0]) {  // proveri ima li pod-ruta
+          switch(this.route.children[0].snapshot.url[0].toString()) {
+          	case 'imovina':
+          		this.izabranaOpcija = 1;
+          		break;
+          	case 'akcije':
+          		this.izabranaOpcija = 2;
+          		break;
+          	case 'grafikoni':
+          		this.izabranaOpcija = 3;
+          		break;
+          	case 'zabeleske':
+          		this.izabranaOpcija = 4;
+          		break;
+          }
+        }
+        else  { // nema pod-ruta, nijedna opcija sa menija nije izabrana
+          this.izabranaOpcija = 0;
+        }
+      } 
+  	});
+  }
+
+  ngOnDestroy() {
+    this.unsubscribeRouterEvents.unsubscribe();
   }
 
 }
